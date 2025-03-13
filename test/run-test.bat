3f5262f8d9b40e2daac81f99a0c75e0c23438a46
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2025-03-13 17:13:57 +0100

    Refactor of CI and solved issues with osx implementation.

--- test/run-test.bat
+++ test/run-test.bat
@@ -1,2 +1,2 @@
-call "c:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %1
-nmake /f Makefile.win32 check clean DLL_CFLAGS=%2 EXE_CFLAGS=%3
+call "c:\Program Files (x86)\Microsoft Visual Studio\%1\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %2
+nmake /f Makefile.win32 check clean DLL_CFLAGS=%3 EXE_CFLAGS=%4
