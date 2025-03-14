1d691713dcf85715b050c5f1b4afe8fea8ca11d6
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2025-03-14 05:55:23 +0100

    Solve windows issues.

--- test/run-test.bat
+++ test/run-test.bat
@@ -1,2 +1,6 @@
-call "c:\Program Files (x86)\Microsoft Visual Studio\%1\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %2
+if "%1" == "2019" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %2
+) else (
+    call "C:\Program Files\Microsoft Visual Studio\%1\Community\VC\Auxiliary\Build\vcvarsall.bat" %2
+)
 nmake /f Makefile.win32 check clean DLL_CFLAGS=%3 EXE_CFLAGS=%4
