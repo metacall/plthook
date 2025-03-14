29ce6e598bb64ff188238c9eaccd35b24877f061
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2025-03-14 06:00:54 +0100

    Add versions again.

--- test/run-test.bat
+++ test/run-test.bat
@@ -1,7 +1,6 @@
 if "%1" == "2019" (
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %2
 ) else (
-    @rem "TODO: This seems not to work, it cannot find the vcvarsall.bat"
-    call "C:\Program Files\Microsoft Visual Studio\%1\Community\VC\Auxiliary\Build\vcvarsall.bat" %2
+    call "C:\Program Files\Microsoft Visual Studio\%1\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %2
 )
 nmake /f Makefile.win32 check clean DLL_CFLAGS=%3 EXE_CFLAGS=%4
