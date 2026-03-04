8c5de1be5baea08573cce2a0f415394ff81953c0
yug <yugagarwal105@gmail.com>
2026-03-05 03:08:16 +0530

    Fix l_base for non-PIE executables using PT_LOAD

--- plthook_elf.c
+++ plthook_elf.c
@@ -271,19 +271,33 @@ static int dl_iterate_cb(struct dl_phdr_info *info, size_t size, void *cb_data)
     if (idx == info->dlpi_phnum) {
         return 0;
     }
+    Elf_Addr real_base = info->dlpi_addr;
+    Elf_Dyn *dynamic = NULL;
     for (idx = 0; idx < info->dlpi_phnum; ++idx) {
         const Elf_Phdr *phdr = &info->dlpi_phdr[idx];
+
+        if (phdr->p_type == PT_LOAD && phdr->p_offset == 0) {
+            real_base = info->dlpi_addr + phdr->p_vaddr;
+        }
+
         if (phdr->p_type == PT_DYNAMIC) {
-            #if __FreeBSD__ >= 13
-            data->lmap.l_addr = (caddr_t)info->dlpi_addr;
-            data->lmap.l_base = (caddr_t)info->dlpi_addr;
-            #else
-            data->lmap.l_addr = info->dlpi_addr;
-            #endif
-            data->lmap.l_ld = (Elf_Dyn*)(info->dlpi_addr + phdr->p_vaddr);
-            return 1;
+            dynamic = (Elf_Dyn*)(info->dlpi_addr + phdr->p_vaddr);
         }
     }
+
+    if (dynamic != NULL) {
+
+    #if __FreeBSD__ >= 13
+        data->lmap.l_addr = (caddr_t)info->dlpi_addr;
+        data->lmap.l_base = (caddr_t)real_base;  
+    #else
+        data->lmap.l_addr = info->dlpi_addr;
+    #endif
+
+        data->lmap.l_ld = dynamic;
+        return 1;
+    }
+
     return 0;
 }
 #endif
