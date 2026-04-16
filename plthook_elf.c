1458346143278132c446eac51c8c5784edbfa1f9
Vicente Eduardo Ferrer Garcia <7854099+viferga@users.noreply.github.com>
2026-04-17 01:35:14 +0200

    Update plthook_elf.c

--- plthook_elf.c
+++ plthook_elf.c
@@ -923,7 +923,7 @@ static void mem_prot_end(mem_prot_iter_t *iter)
 
 static int plthook_open_real(plthook_t **plthook_out, struct link_map *lmap)
 {
-    plthook_t plthook = {NULL, NULL};
+    plthook_t plthook = {0};
     const Elf_Dyn *dyn;
     const char *dyn_addr_base = NULL;
 
